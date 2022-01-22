067aa8d89f80da4734d3330d72adef05930beb94
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-01-22 15:08:31 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const permutationCalcBtn = document.querySelector(".permutation-calc-btn");
+permutationCalcBtn.addEventListener("click", function () {
+  const pObjects = document.querySelector("#permutation-objects").value;
+  const pSample = document.querySelector("#permutation-sample").value;
+  const permutationAnswer = document.querySelector(".permutation-answer");
+
+  if (+pObjects >= +pSample) {
+    let pLastAnswer = factorial(pObjects) / factorial(pObjects - pSample);
+    pLastAnswer == "Infinity" ? (pLastAnswer = factorial(pObjects)) : pLastAnswer;
+    permutationAnswer.innerHTML = showPermutationAns(pObjects, pSample, pLastAnswer);
+  } else {
+    permutationAnswer.innerHTML = `<p style="color:red;">Objects must be bigger than sample</p>`;
+  }
+});
+
+const pForm = document.querySelector(".permutation-form");
+pForm.addEventListener("keyup", function (event) {
+  const pObjects = document.querySelector("#permutation-objects").value;
+  const pSample = document.querySelector("#permutation-sample").value;
+
+  if (event.keyCode === 13 && +pObjects >= +pSample) {
+    event.preventDefault();
+
+    permutationCalcBtn.click();
+  } else if (event.keyCode === 13) {
+    const permutationAnswer = document.querySelector(".permutation-answer");
+    permutationAnswer.innerHTML = `<p style="color:red;">Objects must be bigger than sample</p>`;
+  }
+});
+
+function showPermutationAns(objects, sample, answer) {
+  return `P(n,r) = P(${objects},${sample}) <br>
+  = ${answer}`;
+}
+
+const combinationCalcBtn = document.querySelector(".combination-calc-btn");
+combinationCalcBtn.addEventListener("click", function () {
+  const cObjects = document.querySelector("#combination-objects").value;
+  const cSample = document.querySelector("#combination-sample").value;
+  const combinationAnswer = document.querySelector(".combination-answer");
+
+  if (+cObjects >= +cSample) {
+    let lastAnswer = factorial(cObjects) / (factorial(cSample) * factorial(cObjects - cSample));
+    cSample == 0 ? (lastAnswer = 1) : cSample;
+    cObjects - cSample == 0 ? (lastAnswer = 1) : lastAnswer;
+    combinationAnswer.innerHTML = showCombinationAns(cObjects, cSample, lastAnswer);
+    console.log(lastAnswer);
+  } else {
+    combinationAnswer.innerHTML = `<p style="color:red;">Objects must be bigger than sample</p>`;
+  }
+});
+
+const cForm = document.querySelector(".combination-form");
+cForm.addEventListener("keyup", function (event) {
+  const cObjects = document.querySelector("#permutation-objects").value;
+  const cSample = document.querySelector("#permutation-sample").value;
+
+  if (event.keyCode === 13 && +cObjects >= +cSample) {
+    combinationCalcBtn.click();
+  } else if (event.keyCode === 13) {
+    const combinationAnswer = document.querySelector(".permutation-answer");
+    combinationAnswer.innerHTML = `<p style="color:red;">Objects must be bigger than sample</p>`;
+  }
+});
+
+function showCombinationAns(objects, sample, answer) {
+  return `C(n,r) = C(${objects},${sample}) <br>
+    = ${answer}`;
+}
+
+// factorial function using recursion
+function factorial(num) {
+  if (num != 0) {
+    if (num === 1) {
+      return 1;
+    }
+    return num * factorial(num - 1);
+  } else {
+    return num;
+  }
+}
